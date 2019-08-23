60aa15c433da58bf1e499d0ebe5a781dd3cd5625
TiagoPiovesan <tiago.piovesan.tp@gmail.com>
2019-08-23 19:31:45 -0300

    store images

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -11,14 +11,12 @@ export default async (req, res, next) => {
     }
 
     const [, token] = authHeader.split(' ');
-
     try {
         const decoded = await promisify(jwt.verify)(token, authConfig.secret);
         req.userId = decoded.id;
+
         return next();
     } catch (err) {
         return res.status(401).json({ error: 'Token invalid' });
     }
-
-    return next();
 };
