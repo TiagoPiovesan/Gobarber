944f6e2ab406385d96ba4e59eaad0e6a300ef55f
TiagoPiovesan <tiago.piovesan.tp@gmail.com>
2019-08-26 20:15:06 -0300

    list appointments

--- src/routes.js
+++ src/routes.js
@@ -19,11 +19,9 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
-
 routes.get('/provider', ProviderController.index);
-
 routes.post('/appointments', AppointmentController.store);
-
+routes.get('/appointments', AppointmentController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
