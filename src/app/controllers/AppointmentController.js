329a01ae8273b3897a92beb44247488e30c6806d
TiagoPiovesan <tiago.piovesan.tp@gmail.com>
2019-08-27 16:47:19 -0300

    cancel appointment

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import File from '../models/File';
@@ -76,6 +76,13 @@ class AppointmentController {
             },
         });
 
+        // Check if provider is equal of the user
+
+        if (provider_id === req.userId)
+            return res.status(401).json({
+                error: 'It is not possible, provider and user equal',
+            });
+
         if (checkAvailability) {
             return res
                 .status(400)
@@ -100,6 +107,30 @@ class AppointmentController {
             content: `Novo agendamento de ${user.name} para ${fromattedDate}`,
             user: provider_id,
         });
+
+        return res.json(appointment);
+    }
+
+    async delete(req, res) {
+        const appointment = await Appointment.findByPk(req.params.id);
+
+        if (appointment.user_id !== req.userId) {
+            return res.status(401).json({
+                error: 'You dont have permission to cancel this appointment',
+            });
+        }
+
+        const dateWithSub = subHours(appointment.date, 2);
+
+        if (isBefore(dateWithSub, new Date())) {
+            return res
+                .status(401)
+                .json('You can only cancel appointments 2 hours in advance');
+        }
+
+        appointment.canceled_at = new Date();
+
+        await appointment.save();
 
         return res.json(appointment);
     }
