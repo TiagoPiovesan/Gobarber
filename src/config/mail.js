3e0d3f445b0a4afd8abe5b4b1dfb6c889c949ec0
TiagoPiovesan <tiago.piovesan.tp@gmail.com>
2019-08-27 17:56:48 -0300

    send mail when status of some appointment is true in canceled_at

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,12 +1,19 @@
 export default {
-    host: '',
-    port: '',
+    host: 'smtp.mailtrap.io',
     secure: false,
+    port: 2525,
     auth: {
-        user: '',
-        pass: '',
+        user: '45dbc2c84d302a',
+        pass: 'b4a01d4a756ab6',
     },
     default: {
-        from: 'Equipe Gobarber <noreply...>',
+        from: 'Equipe Gobarber <noreplygobarber.>',
     },
 };
+
+// Amazon SES
+// Mailgun
+// Sparkpost
+// Mandrill ( mailchimp )
+
+// DEV mailtrap
